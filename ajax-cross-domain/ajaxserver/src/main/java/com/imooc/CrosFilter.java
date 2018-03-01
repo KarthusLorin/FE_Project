694d84e7597e1904af0058219fdae5fba7431429
胡彬 <karthuslorin@gmail.com>
2018-03-01 23:42:11 +0800

    add

--- ajax-cross-domain/ajaxserver/src/main/java/com/imooc/CrosFilter.java
+++ ajax-cross-domain/ajaxserver/src/main/java/com/imooc/CrosFilter.java
@@ -1,6 +1,7 @@
 package com.imooc;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -15,11 +16,21 @@ public class CrosFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
 
-        res.addHeader("Access-Control-Allow-Origin", "*");
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        String origin = req.getHeader("Origin");
+
+        if (!org.springframework.util.StringUtils.isEmpty(origin)) {
+            //带cookie的时候，origin必须是全匹配，不能使用*
+            res.addHeader("Access-Control-Allow-Origin", origin);
+        }
+
         res.addHeader("Access-Control-Allow-Methods", "*");
         res.addHeader("Access-Control-Allow-Headers", "Content-Type");
         res.addHeader("Access-Control-Max-Age", "3600");
 
+        // enable cookie
+        res.addHeader("Access-Control-Allow-Credentials", "true");
         chain.doFilter(request, response);
     }
 
--- ajax-cross-domain/ajaxserver/src/main/java/com/imooc/TestController.java
+++ ajax-cross-domain/ajaxserver/src/main/java/com/imooc/TestController.java
@@ -16,6 +16,12 @@ public class TestController {
     public ResultBean postJson(@RequestBody User user) {
         System.out.println("TestController.postJson()");
 
-        return new ResultBean("postJson" + user.getName());
+        return new ResultBean("postJson " + user.getName());
+    }
+
+    @GetMapping("/getCookie")
+    public ResultBean getCookie(@CookieValue(value="cookie1") String cookie1) {
+        System.out.println("TestController.getCookie()");
+        return new ResultBean("getCookie " + cookie1);
     }
 }
